fc135f225c5e316b9af71bb5eab86127e2db644f
Farha <farhasalim@gmail.com>
2025-01-30 19:31:51 +0530

    Created Django Super user with command line. Made changes to admin.py

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from profiles_api import models
+
+admin.site.register(models.UserProfile) # Tells django admin to register our userprofile model with the admin site. 
+
