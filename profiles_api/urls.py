f1a01e5ce3f1a8280ca7341e07dff5d739755cff
Farha <farhasalim@gmail.com>
2025-02-02 14:39:47 +0530

    Added viewsets to profiles API - and the functions retrieve, update, partial_update, and destroy

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path ('hello-view/', views.HelloApiView.as_view()),
+    path ('', include(router.urls)) #As you register new routes with our router above, it generates 
+    # a new list of urls associated for viewset. It will figure out URLs for all functions we add to our viewset
+    # and then generates urls list which we can pass in with the path and include. 
+    # Blank string = since we don't want to add a prefix to the URLS
 ]
